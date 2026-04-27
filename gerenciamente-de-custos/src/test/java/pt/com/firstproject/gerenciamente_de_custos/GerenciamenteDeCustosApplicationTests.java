e5e6d9361a4a85f0867019ffd9c77afb4d4ad408
Adalberto-Junior <86660513+Adalberto-Junior@users.noreply.github.com>
2026-04-27 23:38:51 +0100

    firts step -> configuraction

---
+++ gerenciamente-de-custos/src/test/java/pt/com/firstproject/gerenciamente_de_custos/GerenciamenteDeCustosApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.com.firstproject.gerenciamente_de_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamenteDeCustosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
